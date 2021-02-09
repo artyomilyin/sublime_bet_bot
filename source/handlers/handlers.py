90369da51b5052e067db14f92693a888e4e5cb52
Artyom ILYIN <artyom_ilyin@mail.ru>
2021-02-09 12:25:03 +0300

    edit welcome message

--- source/handlers/handlers.py
+++ source/handlers/handlers.py
@@ -13,7 +13,7 @@ async def send_welcome(message: types.Message):
     """
     This handler will be called when user sends `/start` or `/help` command
     """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
+    await message.reply("Hi!\nI'm EchoBot! (for now)")
 
 
 @dp.message_handler()
