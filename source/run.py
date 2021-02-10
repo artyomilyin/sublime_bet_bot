befc498f65078daf3e10164b4d5907b21c027c07
Artyom Ilyin <aailyin@bi-telco.com>
2021-02-10 14:13:11 +0300

    indentation fix

--- source/run.py
+++ source/run.py
@@ -8,7 +8,7 @@ from handlers.handlers import dp
 DEBUG = os.environ.get('LOCAL_DEBUG', False)
 logging_level = logging.DEBUG if DEBUG else logging.INFO
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                     level=logging_level)
+                    level=logging_level)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
